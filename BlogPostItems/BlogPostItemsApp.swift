8909b1c294003af29d0c91430b7b64886f17365e
Wes Matlock <wmatlock@gmail.com>
2024-06-19 14:10:42 -0600

    Initial Commit

---
+++ BlogPostItems/BlogPostItemsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlogPostItemsApp.swift
+//  BlogPostItems
+//
+//  Created by Wesley Matlock on 6/19/24.
+//
+
+import SwiftUI
+
+@main
+struct BlogPostItemsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlogPostItemsTests/BlogPostItemsTests.swift
@@ -0,0 +1,16 @@
+//
+//  BlogPostItemsTests.swift
+//  BlogPostItemsTests
+//
+//  Created by Wesley Matlock on 6/19/24.
+//
+
+import Testing
+
+struct BlogPostItemsTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
