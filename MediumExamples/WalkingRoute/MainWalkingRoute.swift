144893d7ddf5b93ee26967c5635b26aa19ffd564
Wes Matlock <wmatlock@gmail.com>
2024-09-13 15:22:32 -0600

    [WIP] Tracking starting to take place

---
+++ MediumExamples/WalkingRoute/MainWalkingRoute.swift
@@ -0,0 +1,18 @@
+//
+//  MainWalkingRoute.swift
+//  BlogPostItems
+//
+//  Created by Wesley Matlock on 9/13/24.
+//
+
+import SwiftUI
+
+struct MainWalkingRoute: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainWalkingRoute()
+}
---
+++ MediumExamples/WalkingRoute/RouterMapper.swift
@@ -0,0 +1,18 @@
+//
+//  RouterMapper.swift
+//  BlogPostItems
+//
+//  Created by Wesley Matlock on 9/13/24.
+//
+
+import SwiftUI
+
+struct RouterMapper: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RouterMapper()
+}
